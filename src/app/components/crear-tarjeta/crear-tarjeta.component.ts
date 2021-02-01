c8db163cc33d5e71d1dd7a31c15b2200164a6708
truizdiaz <tomiruizdiaz@gmail.com>
2021-02-01 09:45:19 -0300

    Crear Tarjeta Validaciones

--- src/app/components/crear-tarjeta/crear-tarjeta.component.ts
+++ src/app/components/crear-tarjeta/crear-tarjeta.component.ts
@@ -12,9 +12,9 @@ export class CrearTarjetaComponent implements OnInit {
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
       titular: ['', Validators.required],
-      numeroTarjeta: ['', Validators.required],
-      fechaExpiracion: ['', Validators.required],
-      cvv: ['', Validators.required],
+      numeroTarjeta: ['', [Validators.required, Validators.minLength(16), Validators.maxLength(16)]],
+      fechaExpiracion: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(5)]],
+      cvv: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(3)]],
     })
    }
 
