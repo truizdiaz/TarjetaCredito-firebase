fa16e172d4f0ea22ace53700016f0f0c530c303a
truizdiaz <tomiruizdiaz@gmail.com>
2021-02-01 14:32:57 -0300

    Ordenamos la grilla

--- src/app/components/listar-tarjeta/listar-tarjeta.component.ts
+++ src/app/components/listar-tarjeta/listar-tarjeta.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TarjetaCredito } from 'src/app/models/TarjetaCredito';
+import { TarjetaService } from 'src/app/services/tarjeta.service';
 
 @Component({
   selector: 'app-listar-tarjeta',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar-tarjeta.component.css']
 })
 export class ListarTarjetaComponent implements OnInit {
+  listTarjetas: TarjetaCredito[] = [];
 
-  constructor() { }
+  constructor(private _tarjetaService: TarjetaService) { }
 
   ngOnInit(): void {
+    this.obtenerTarjetas();
+  }
+
+  obtenerTarjetas() {
+    this._tarjetaService.obtenerTarjetas().subscribe(doc => {
+      this.listTarjetas = [];
+      doc.forEach((element: any) => {
+        this.listTarjetas.push({
+          id: element.payload.doc.id,
+          ...element.payload.doc.data()
+        });
+      });
+      console.log(this.listTarjetas);
+    })
   }
 
 }
--- src/app/services/tarjeta.service.ts
+++ src/app/services/tarjeta.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 import { TarjetaCredito } from '../models/TarjetaCredito';
 
 @Injectable({
@@ -11,5 +12,9 @@ export class TarjetaService {
 
   guardarTarjeta(tarjeta: TarjetaCredito): Promise<any> {
     return this.firebase.collection('tarjetas').add(tarjeta);
+  }
+
+  obtenerTarjetas(): Observable<any> {
+    return this.firebase.collection('tarjetas', ref => ref.orderBy('fechaCreacion', 'asc')).snapshotChanges();
   }
 }
